d7260148e9788bf86cb037e79db2b5c0ad586758
flof <evanrose99@gmail.com>
2024-01-18 18:29:49 -0500

    continued working on blackjack and card/deck class structures

--- src/main/java/local/Casino/Blackjack/Blackjack.java
+++ src/main/java/local/Casino/Blackjack/Blackjack.java
@@ -1,9 +1,24 @@
 package main.java.local.Casino.Blackjack;
 
+import main.java.local.Casino.Blackjack.Card;
+import main.java.local.Casino.Blackjack.Deck;
+
+int DEALER_MIN_STAND = 18;
+
 public class Blackjack {
 
     Account player;
+    Deck deck;
+    Card[] dealersCards;
+    Card[] playerCards;
     
+    public Blackjack(Account player) {
+        this.player = player;
+        deck = new Deck();
+        deck.shuffle();
+
+
+    }
 
 
 }
--- src/main/java/local/Casino/Blackjack/Deck.java
+++ src/main/java/local/Casino/Blackjack/Deck.java
@@ -3,6 +3,26 @@ package main.java.local.Casino.Deck;
 public class Deck {
 
     public Card[] MainDeck;
+    private int numCards = 52;
 
+    public Deck() {
+       resetDeck();
+    }
+
+    public void resetDeck() {
+        
+    }
+
+    public void shuffle() {
+
+    }
+
+    // deals the top card
+    public Card deal() {
+        if (numCards > 0) {
+            return MainDeck[--numCards]
+        }
+        return null;
+    }
 
 }
