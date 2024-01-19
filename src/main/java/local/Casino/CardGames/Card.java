0dd159f8eed987da9a7b14f573c2bd5c7a08aa22
flof <evanrose99@gmail.com>
2024-01-19 17:20:15 -0500

    shuffle/reset deck and bug fixes

--- src/main/java/local/Casino/CardGames/Card.java
+++ src/main/java/local/Casino/CardGames/Card.java
@@ -1,38 +1,36 @@
 package main.java.local.Casino.CardGames;
 
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
-enum Suit {
-    HEARTS,
-    SPADES,
-    DIAMONDS,
-    CLUBS;
-
-    private static final List<Suit> SUITS = Collections.unmodifiableList(Arrays.asList(values()));
-}
-
-enum Value {
-    ONE,
-    TWO,
-    THREE,
-    FOOUR,
-    FIVE,
-    SIX,
-    SEVEN,
-    EIGHT,
-    NINE,
-    TEN,
-    JACK,
-    QUEEN,
-    KING,
-    ACE;
-
-    private static final List<Value> VALUES = Collections.unmodifiableList(Arrays.asList(values()));
-}
 
 public class Card {
+
+    public static enum Suit {
+        HEARTS,
+        SPADES,
+        DIAMONDS,
+        CLUBS;
+
+        public static final Suit[] SUITS = values();
+    }
+
+    public static enum Value {
+        ONE,
+        TWO,
+        THREE,
+        FOOUR,
+        FIVE,
+        SIX,
+        SEVEN,
+        EIGHT,
+        NINE,
+        TEN,
+        JACK,
+        QUEEN,
+        KING,
+        ACE;
+
+        public static final Value[] VALUES = values();
+    }
 
     public final Value cardValue;
     public final Suit cardSuit;
--- src/main/java/local/Casino/CardGames/Deck.java
+++ src/main/java/local/Casino/CardGames/Deck.java
@@ -4,25 +4,39 @@ import java.util.Random;
 
 public class Deck {
 
-    public Card[] MainDeck;
+    public Card[] mainDeck;
     private int numCards = 52;
 
     public Deck() {
-       resetDeck();
+        mainDeck = new Card[52];
+        resetDeck();
     }
 
     public void resetDeck() {
-        
+        numCards = 52;
+        int pos = 0;
+        for (int s = 0; s < Card.Suit.SUITS.length; s++) {
+            for (int v = 0; v < Card.Value.VALUES.length; v++) {
+                mainDeck[pos++] = new Card(Card.Value.VALUES[s], Card.Suit.SUITS[v]);
+            }
+        }
     }
 
     public void shuffle() {
-
+        // https://www.digitalocean.com/community/tutorials/shuffle-array-java
+        Random rand = new Random();
+		for (int i = 0; i < mainDeck.length; i++) {
+			int randomIndexToSwap = rand.nextInt(mainDeck.length);
+			Card temp = mainDeck[randomIndexToSwap];
+			mainDeck[randomIndexToSwap] = mainDeck[i];
+			mainDeck[i] = temp;
+		}
     }
 
     // deals the top card
     public Card deal() {
         if (numCards > 0) {
-            return MainDeck[--numCards];
+            return mainDeck[--numCards];
         }
         return null;
     }
