156629a0d6fe2ec6b7aa1a01a95c66b2d82566e8
flof <evanrose99@gmail.com>
2024-02-28 14:25:59 -0500

    initial slots api changes

--- src/main/java/local/API/Main.java
+++ src/main/java/local/API/Main.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import local.Casino.Slots.Slots;
+
 import java.sql.* ;  // for standard JDBC programs
 
 @SpringBootApplication
@@ -36,99 +38,156 @@ public class Main {
 		return "CooperCasino (Ping: Sucsessful)\n";
 	}
 
-	@GetMapping("/pp")
-	public String pp() {
-		return "CooperCasino pp (Status: Online)\n";
-	}
-
 	String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
 	String USER = "postgres";
 	String PASS = "password";
-	String QUERY = "INSERT INTO slots_symbols(symbol_id, symbol_name) VALUES (69, 'TEST SYMBOL FOR JDBC');";
-	String Q2 = "DELETE FROM slots_symbols WHERE symbol_id = 69;";
 
-	@GetMapping("/Test")
-	public String test() {
-		// return "hello";
 
+	// String QUERY = "INSERT INTO slots_symbols(symbol_id, symbol_name) VALUES (69, 'TEST SYMBOL FOR JDBC');";
+	// String Q2 = "DELETE FROM slots_symbols WHERE symbol_id = 69;";
+	// @GetMapping("/Test")
+	// public String test() {
+	// 	// return "hello";
+
+	// 	try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+	// 		Statement stmt = conn.createStatement();
+	// 		ResultSet rs = stmt.executeQuery(QUERY);
+	// 		Statement stmt2 = conn.createStatement();
+	// 		ResultSet rs2 = stmt2.executeQuery(Q2);) {
+	// 	} catch (SQLException e) {
+	// 		e.printStackTrace();
+	// 	} 
+	// 	return "CooperCasino (Status: Online)\n";
+	// }
+
+	boolean isValidAccount(int userID) {
+		String QUERY = "SELECT COUNT(1) FROM postgres.public.user WHERE user_id = "+userID+";";
 		try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
 			Statement stmt = conn.createStatement();
-			ResultSet rs = stmt.executeQuery(QUERY);
-			Statement stmt2 = conn.createStatement();
-			ResultSet rs2 = stmt2.executeQuery(Q2);) {
+			ResultSet rs = stmt.executeQuery(QUERY);) {
+			return (rs.getInt("count") == 1);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} 
-		return "CooperCasino (Status: Online)\n";
+		return false;
 	}
 
-	private boolean isValidGame(String game) {
-		switch (game) {
-			case "Slots":
-				return true;
-			case "Blackjack":
-				return true;
-			default:
-				return false;
-		}
+	boolean isValidBet(int userID, int bet) {
+		return true;
 	}
 
-	final int INVALID_INPUT = 400; //not sure what the error code is for this 
-	final int OK = 200;
-
-	String createBlackjack(int id, int bet) {
-		return OK+": RETURNING FROM createBlackjack()";
-	}
-
-	String playSlots(int id, int bet) {
-		// roll slots
-		// put stuff in database
-		// return information
-		return OK+": RETURNING FROM playSlots()";
-	}
-
-
-	@GetMapping("/NewGame")
-	public String newGame(	@RequestParam(value = "userID", defaultValue = "-1") String userID,
-							@RequestParam(value = "game", defaultValue = "-1") String game,
+	@GetMapping("/PlaySlots")
+	public String playSlots(@RequestParam(value = "userID", defaultValue = "-1") String userID,
 							@RequestParam(value = "bet", defaultValue = "-1") String bet) {
-		if (!isValidGame(game)) {
-			return INVALID_INPUT+": Invalid Game Type";
-		}
+		// if (isValidAccount(Integer.parseInt(userID))) {
+		// 	return "true";
+		// }
+		// return "false";
 
-		int id = -1;
-		int betAmount = -1;
+		String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
+		String USER = "postgres";
+		String PASS = "password";
+		String QUERY = "INSERT INTO slots_symbols(symbol_id, symbol_name) VALUES (69, 'TEST SYMBOL FOR JDBC');";
 
-		try {
-			id = Integer.parseInt(userID);
-		} catch(Exception e) {
-			return INVALID_INPUT+": Invalid User ID";
-		};
+		try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(QUERY);) {
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return "e";
+		} 
 
-		try {
-			betAmount = Integer.parseInt(bet);
-		} catch(Exception e) {
-			return INVALID_INPUT+": Invalid Bet";
-		};
+		return "e2";
 
-		if (id < 0) {
-						return INVALID_INPUT+": Invalid User ID";
-		}
+		// String QUERY = "SELECT COUNT(1) FROM public.\"user\" WHERE user_id = "+userID+";";
+		// try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+		// 	Statement stmt = conn.createStatement();
+		// 	/* ResultSet rs = stmt.executeQuery(QUERY); */) {
+		// 	// return ""+rs.getInt(1);
+		// 	return "didn't error";
+		// } catch (SQLException e) {
+		// 	System.out.println("\n\n\n\n\n\n");
+		// 	e.printStackTrace();
+		// } 
+		// return "error";
 
-		if (betAmount < 0) {
-						return INVALID_INPUT+": Invalid Bet";
-		}
 
 
-		if (game.equals("Slots")) {
-			return playSlots(id, betAmount);
-		}
-
-		if (game.equals("Blackjack")) {
-			return createBlackjack(id, betAmount);
-		}
-		return OK+": RETURNING FROM newGame()";
+		// 1. Is Valid Account
+		// 2. Balance >= Bet
+		// 3. Generate Roll
+		// 4. Get Payout From DB
+		// 5. Update Database (userBalance, slotsHistory)...
+		// 6. Return Payout, Payout_ID, CODE
 	}
+
+	@GetMapping("/NewBlackjack")
+	public String newBlackjack() {
+		return "1";
+	}
+
+	@GetMapping("/UpdateBlackjack")
+	public String updateBlackjack() {
+		return "2";
+	}
+
+	@GetMapping("/RejoinBlackjack")
+	public String rejoinBlackjack() {
+		return "3";
+	}
+
+	// private boolean isValidGame(String game) {
+	// 	switch (game) {
+	// 		case "Slots":
+	// 			return true;
+	// 		case "Blackjack":
+	// 			return true;
+	// 		default:
+	// 			return false;
+	// 	}
+	// }
+
+	// @GetMapping("/NewGame")
+	// public String newGame(	@RequestParam(value = "userID", defaultValue = "-1") String userID,
+	// 						@RequestParam(value = "game", defaultValue = "-1") String game,
+	// 						@RequestParam(value = "bet", defaultValue = "-1") String bet) {
+	// 	if (!isValidGame(game)) {
+	// 		return INVALID_INPUT+": Invalid Game Type";
+	// 	}
+
+	// 	int id = -1;
+	// 	int betAmount = -1;
+
+	// 	try {
+	// 		id = Integer.parseInt(userID);
+	// 	} catch(Exception e) {
+	// 		return INVALID_INPUT+": Invalid User ID";
+	// 	};
+
+	// 	try {
+	// 		betAmount = Integer.parseInt(bet);
+	// 	} catch(Exception e) {
+	// 		return INVALID_INPUT+": Invalid Bet";
+	// 	};
+
+	// 	if (id < 0) {
+	// 					return INVALID_INPUT+": Invalid User ID";
+	// 	}
+
+	// 	if (betAmount < 0) {
+	// 					return INVALID_INPUT+": Invalid Bet";
+	// 	}
+
+
+	// 	if (game.equals("Slots")) {
+	// 		return playSlots(id, betAmount);
+	// 	}
+
+	// 	if (game.equals("Blackjack")) {
+	// 		return createBlackjack(id, betAmount);
+	// 	}
+	// 	return OK+": RETURNING FROM newGame()";
+	// }
 
 }
 
