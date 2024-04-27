6c89e3dd8b34d6b10a5770317b34386c717dec16
James Ryan <james.paul9889@gmail.com>
2024-04-27 15:42:54 -0400

    Updated rouletteBetPair to match changes.

--- src/main/java/local/Casino/Roulette/RouletteBetPair.java
+++ src/main/java/local/Casino/Roulette/RouletteBetPair.java
@@ -1,9 +1,9 @@
 package local.Casino;
 
 public class RouletteBetPair {
-	public String betNum;
-	public Float betValue;
-	public String betType;
+	public String[] betNum; // LUMP (reds, dozen, etc), 1.2, 1, 37, etc
+	public Float betValue; // vbucks bet
+	public String betType; //single, LUMP, split, etc
 
 	public RouletteBetPair(String bN, float bV, String bT){
 		betNum = bN;
